f745a268486348b7e8027f30a7aa6b7fd7cc16cc
David Ang <davidang09@gmail.com>
2014-06-29 12:53:51 +0800

    migration script to store easy post postage label data

---
+++ db/migrate/20140629035855_create_postage_labels.rb
@@ -0,0 +1,22 @@
+class CreatePostageLabels < ActiveRecord::Migration
+  def change
+    create_table :spree_postage_labels do |t|
+      t.references :shipment
+
+      t.string    :easypost_id
+      t.integer   :date_advance      
+      t.string    :integrated_form      
+      t.datetime  :label_date
+      t.integer   :label_resolution
+      t.string    :label_size
+      t.string    :label_type
+      t.string    :label_file_type
+      t.string    :label_url
+      t.string    :label_pdf_url
+      t.string    :label_epl2_url
+      t.string    :label_zpl_url
+
+      t.timestamps
+    end
+  end
+end
