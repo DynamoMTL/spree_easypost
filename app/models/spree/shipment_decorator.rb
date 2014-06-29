227934b2557dbf4427a4e30d87ee1e1fa09e0b31
David Ang <davidang09@gmail.com>
2014-06-29 12:55:01 +0800

    create a new postage label and assign to current shipment upon buying an easy post shipment

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,5 +1,6 @@
 Spree::Shipment.class_eval do
   state_machine.before_transition :to => :shipped, :do => :buy_easypost_rate
+  has_one :postage_label
 
   def tracking_url
     nil # TODO: Work out how to properly generate this
@@ -30,7 +31,21 @@ Spree::Shipment.class_eval do
       rate.id == selected_easy_post_rate_id
     end
 
-    easypost_shipment.buy(rate)
+    easypost_shipment.buy(rate)    
     self.tracking = easypost_shipment.tracking_code
+
+    create_postage_label(easypost_shipment)    
+  end
+
+  def create_postage_label(shipment)   
+    response_attributes = JSON.parse(shipment.postage_label.to_s)
+
+    postage_label = ::Spree::PostageLabel.new
+    # assign attributes to our new postage label, ignoring those that are not defined on our model
+    postage_label.attributes = response_attributes.reject{|k,v| !postage_label.attributes.keys.member?(k.to_s) || k.to_s == 'id' }      
+    postage_label.easypost_id = response_attributes['id']
+    # assign this to the current shipment
+    postage_label.shipment = self
+    postage_label.save
   end
 end
