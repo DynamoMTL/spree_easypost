fb7b819bf34cd7e4165684a266cf16e57684f79e
David Ang <davidang09@gmail.com>
2014-08-12 03:15:46 +0800

    remove duplicate tracking details when there's multiple delivery location

--- app/overrides/add_tracking_details_to_orders_edit.rb
+++ app/overrides/add_tracking_details_to_orders_edit.rb
@@ -4,20 +4,24 @@ Deface::Override.new(:virtual_path => 'spree/admin/orders/_shipment',
   :text => '
     <fieldset class="no-border-bottom">
     <legend align="center">Tracking Details</legend>
+    <% ep_shipment = ::Spree::EasyPost::Shipment.where(order_id: order.id, shipment_id: shipment.id).first %>
     <% events = ::Spree::EasyPost::Event.where(order_id: order.id).order(created_at: :desc) %>
     <table>
     <thead>
       <tr>
+        <th>Tracking Code</th>
         <th>Date</th>
-        <th>Status</th>
         <th>Mode</th>
+        <th>Status</th>
         <th>Details</th>
       </tr>
     </thead>
     <% events.each do |event| %>
       <% result = JSON.parse(event.result) %>
+      <% next unless result["shipment_id"] == ep_shipment.easypost_id %>
       <% tracking_details = event.result["tracking_details"] %>
       <tr>
+        <td><%= result["tracking_code"] %></td>
         <td><%= DateTime.parse(result["created_at"]).strftime("%D") %></td>
         <td><%= result["mode"] %></td>
         <td><%= result["status"] %></td>
