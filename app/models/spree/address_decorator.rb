153af5ffb1b8dec5c080b7f08fc0a5222e2201b5
Josh Nussbaum <joshnuss@gmail.com>
2015-04-11 21:02:26 -0400

    Refactor to class

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -9,33 +9,7 @@ module Spree::AddressDecorator
 
 protected
   def validate_address
-    response = EasyPost::Address.create_and_verify(easy_post_attributes)
-
-    if response.message.present?
-      errors.add(:base, response.message)
-    else
-      self.address1 = response.street1
-      self.address2 = response.street2
-      self.city = response.city
-      self.state = Spree::State.find_by_abbr(response.state) unless state.try(:abbr) == response.state
-      self.zipcode = response.zip
-    end
-
-  rescue EasyPost::Error => e
-    errors.add(:base, e.message)
-  end
-
-  def easy_post_attributes
-    {
-      name: full_name,
-      street1: address1,
-      street2: address2,
-      city: city,
-      state: state.try(:abbr) || state_name,
-      zip: zipcode,
-      country: country.try(:iso),
-      phone: phone
-    }
+    Spree::EasyPost::AddressVerification.new(self).verify!
   end
 end
 
